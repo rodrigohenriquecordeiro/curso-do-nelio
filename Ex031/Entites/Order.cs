cea54b3493a7ae81a6803866d166dfa17b2c1247
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2022-08-20 11:16:29 -0300

    Exercício de Fixação número 31

---
+++ Ex031/Entites/Order.cs
@@ -0,0 +1,51 @@
+﻿using Ex031.Entites.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex031.Entites
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (var item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            return $"Client: {Client.Name} ({Client.birthDate}) - {Client.Email}";
+        }
+    }
+}
---
+++ Ex031/Program.cs
@@ -0,0 +1,59 @@
+﻿using Ex031.Entites;
+using Ex031.Entites.Enuns;
+using System;
+
+namespace Ex031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: "); string name = Console.ReadLine().Trim();
+            Console.Write("Email: "); string email = Console.ReadLine().Trim();
+            Console.Write("Birth Date (DD/MM/YYYY): "); DateTime dateBirth = DateTime.Parse(Console.ReadLine().Trim());
+            Client client = new Client(name, email, dateBirth);
+
+            Console.WriteLine("\nEnter order data");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("  [0] Pending Payment");
+            Console.WriteLine("  [1] Processing");
+            Console.WriteLine("  [2] Shipped");
+            Console.WriteLine("  [3] Delivered");
+            Console.WriteLine("-----------------------");
+            Console.Write("Your chose: ");
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine().Trim());
+            Order order = new Order(DateTime.Now, orderStatus, client);
+
+            Console.Write("\nHow many items to this order? "); int num = int.Parse(Console.ReadLine().Trim());
+            Console.WriteLine();
+
+            OrderItem orderItem;
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product Name: "); string productName = Console.ReadLine().Trim();
+                Console.Write("Product Price: "); double productPrice = double.Parse(Console.ReadLine().Trim());
+                Console.Write("Quantity: "); int quantity = int.Parse(Console.ReadLine());
+                Product product = new Product(productName, productPrice);
+                orderItem = new OrderItem(quantity, productPrice, product);
+                order.AddItem(orderItem);
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("ORDER SUMARY:");
+            Console.WriteLine($"\nOrder moment: {order.Moment}");
+            Console.WriteLine($"Order status: {order.Status}");
+            Console.WriteLine(order);
+            Console.WriteLine("\nOrder items:");
+            foreach (var item in order.Items)
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine($"{item.Product.Name}, Quantity: {item.Quantity}, Subtotal: {item.SubTotal():N}");
+            }
+            Console.WriteLine($"\nTotal price: {order.Total():N}");
+
+            Console.ReadLine();
+        }
+    }
+}
