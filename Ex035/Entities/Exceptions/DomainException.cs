081e933e98ed46ca03421b208821c4f85a29430f
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2022-08-28 09:00:50 -0300

    Exercício de Fixação número 33

---
+++ Ex035/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ex034.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        { 
+        }
+    }
+}
---
+++ Ex035/Program.cs
@@ -0,0 +1,42 @@
+﻿using Ex034.Entities;
+using Ex034.Entities.Exceptions;
+using System;
+
+namespace Ex034
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: "); int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (DD/MM/YYYY): "); DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (DD/MM/YYYY): "); DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine($"Reservation: {reservation}");
+
+                Console.WriteLine();
+                Console.WriteLine("Enter date to update the reservation");
+                Console.Write("Check-in date (DD/MM/YYYY): "); checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (DD/MM/YYYY): "); checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDate(checkIn, checkOut);
+                Console.WriteLine($"Reservation: {reservation}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Error in reservation: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Format error: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error: {e.Message}");
+            }
+        }
+    }
+}
